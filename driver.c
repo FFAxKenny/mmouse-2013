68ad9f3785d22e02cd48e3eee908fd6941ca7c41
Kenny Luong <luong97@hawaii.edu>
2013-02-16 17:16:53 -1000

    driver working with one LED and internal clock on RB4 AB4

--- driver.c
+++ driver.c
@@ -10,30 +10,67 @@
 #include <xc.h>
 #include <dsp.h>
 
-// Select Internal FRC at POR
+
 _FOSCSEL(FNOSC_FRC & IESO_OFF);
-// Enable Clock Switching and Configure Primary Oscillator in XT mode
 _FOSC(FCKSM_CSECMD & OSCIOFNC_OFF & POSCMD_NONE);
 
 int main()
 {
-    // Configure PLL prescaler, PLL postscaler, PLL divisor
-    PLLFBD=63;
-    // M=65
-    CLKDIVbits.PLLPOST=0;
-    // N2=2
-    CLKDIVbits.PLLPRE=1;
-    // N1=3
+    int i = 0;
+    int number = 0;
 
-    // Wait for PLL to lock
-    while (OSCCONbits.LOCK!= 1);
+    LATBbits.LATB4=0;
+    LATAbits.LATA4=0;
+
+    ANSELA = 0;//configures pin B4 as digital
+    ANSELB = 0;//configures pin B4 as digital
+
+    TRISA = 0b00000000;      // Configure B Ports as output
+    TRISB = 0b00000000;      // Configure B Ports as output
+
+
+    /*
+    // Configure PLL prescaler, PLL postscaler, PLL divisor
+    PLLFBD=63;              // M=65
+    CLKDIVbits.PLLPOST=0;   // N2=2
+    CLKDIVbits.PLLPRE=1;    // N1=3
 
     // Initiate Clock Switch to FRC oscillator with PLL (NOSC=0b001)
     __builtin_write_OSCCONH(0x01);
     __builtin_write_OSCCONL(OSCCON | 0x01);
+
+    LATBbits.LATB4=1;
+    LATAbits.LATA4=1;
+
     // Wait for Clock switch to occur
     while (OSCCONbits.COSC!= 0b001);
+    
+
     // Wait for PLL to lock
     while (OSCCONbits.LOCK!= 1);
+    */
+
+    while(1)
+    {
+        i=0;
+        while(i<30000) {
+            LATBbits.LATB4 = 1;
+            LATAbits.LATA4 = 1;
+            i++;
+        }
+
+
+        i = 0; 
+        while(i<30000) {
+            LATBbits.LATB4 = 0;
+            LATAbits.LATA4 = 0;
+            i++;
+        }
+
+    }
+    while(1);
+
+
+
 
 }
