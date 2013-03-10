53901a7f62a4150293a1a46b108fa205328b41dd
Kenny Luong <luong97@hawaii.edu>
2013-03-09 18:55:36 -1000

    changed driver.c to recent motor

--- TestBenches/MaxAccelTestBench.X/driver.c
+++ TestBenches/MaxAccelTestBench.X/driver.c
@@ -15,13 +15,17 @@ _FOSCSEL(FNOSC_FRC & IESO_OFF);
 _FOSC(FCKSM_CSECMD & OSCIOFNC_OFF & POSCMD_NONE);
 
 long i = 0;
+int motor_pulse = 0;
+int test;
 int main()
 {
-    long i = 0;
+    long k = 40000;
     //int number = 0;
 
-    LATBbits.LATB4=0;
-    LATAbits.LATA4=0;
+    LATBbits.LATB9=1;
+    LATBbits.LATB6=1;
+    LATBbits.LATB5=1;
+    LATAbits.LATA4=1;
 
     
     ANSELA = 0;//configures pin B4 as digital
@@ -35,8 +39,8 @@ int main()
      *      Timer1 Configuration
      ********************************/
     T1CON = 0;               // Reset T1 Configuration
-    T1CONbits.TCKPS = 3;     // Set the ratio to the highest
-    PR1 = 50000;             // Set the timer
+    T1CONbits.TCKPS = 1;     // Set the ratio to the highest
+    PR1 = 10000;             // Set the timer
 
     _T1IP = 1;
     _T1IF = 0;
@@ -70,13 +74,18 @@ int main()
     /********************************
      *      Main Body
      ********************************/
-
+  
     while(1)
     {
-            __delay32(50000);
-            LATAbits.LATA4 = 1;
-            __delay32(50000);
-            LATAbits.LATA4 = 0;
+        /*
+            __delay32(k);
+            LATBbits.LATB9 = 1;
+            LATBbits.LATB8 = 1;
+            __delay32(k);
+            LATBbits.LATB9 = 0;
+            LATBbits.LATB8 = 0;
+            if(k>10000) k=k-100;
+         */
     }
 
 
@@ -85,9 +94,39 @@ int main()
 
 void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void)
 {
-        
         _T1IF = 0;      // Reset the timer flag
+
+        if(motor_pulse==0)
+            motor_pulse=1;
+        else
+            motor_pulse=0;
+
+        LATBbits.LATB9 = motor_pulse;
+        LATBbits.LATB8 = motor_pulse;
         
+        if(PR1 > 3000)
+            PR1 = PR1 - 4;             // Set the timer
+
+        /*
+        else if(PR1 > 700)
+        {
+            PR1 = PR1 - 1;
+        }
+        else if(PR1 > 300)
+        {
+            if( test == 500 )
+            {
+                PR1 = PR1-1;
+                test = 0;
+            }
+            else
+            {
+                test++;
+            }
+        }
+         */
+         
+
         // The body of the Timer1 Interrupt goes here
 
 }
