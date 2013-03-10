ff26f7c9684e4316f47949e58ec0de8d32a418a8
Kenny Luong <luong97@hawaii.edu>
2013-03-09 19:35:32 -1000

    moved setup to seperate functions

--- TestBenches/ConfigurationTest.X/driver.c
+++ TestBenches/ConfigurationTest.X/driver.c
@@ -27,48 +27,11 @@ int main()
     LATBbits.LATB5=1;
     LATAbits.LATA4=1;
 
-    
     ANSELA = 0;//configures pin B4 as digital
     ANSELB = 0;//configures pin B4 as digital
 
     TRISA = 0b00000000;      // Configure B Ports as output
     TRISB = 0b00000000;      // Configure B Ports as output
-
- 
-    /********************************
-     *      Timer1 Configuration
-     ********************************/
-    T1CON = 0;               // Reset T1 Configuration
-    T1CONbits.TCKPS = 1;     // Set the ratio to the highest
-    PR1 = 10000;             // Set the timer
-
-    _T1IP = 1;
-    _T1IF = 0;
-    _T1IE = 1;
-    T1CONbits.TON = 1;       // Enable Timer
-
-    /********************************
-     *      PLL Configuration
-     ********************************/
-    // Configure PLL prescaler, PLL postscaler, PLL divisor
-    PLLFBD=63;              // M=65
-    CLKDIVbits.PLLPOST=0;   // N2=2
-    CLKDIVbits.PLLPRE=1;    // N1=3
-
-    // Initiate Clock Switch to FRC oscillator with PLL (NOSC=0b001)
-    __builtin_write_OSCCONH(0x01);
-    __builtin_write_OSCCONL(OSCCON | 0x01);
-
-
-    // This bit of code doesn't work for some reason..
-    /**************************************
-        // Wait for Clock switch to occur
-        while (OSCCONbits.COSC!= 0b001);
-        
-
-        // Wait for PLL to lock
-        while (OSCCONbits.LOCK!= 1);
-    ***************************************/
 
 
     /********************************
@@ -128,6 +91,50 @@ void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void)
          
 
         // The body of the Timer1 Interrupt goes here
+
+}
+
+void setupTimer1(void)
+{
+    /********************************
+     *      Timer1 Configuration
+     ********************************/
+    T1CON = 0;               // Reset T1 Configuration
+    T1CONbits.TCKPS = 1;     // Set the ratio to the highest
+    PR1 = 10000;             // Set the timer
+
+    _T1IP = 1;
+    _T1IF = 0;
+    _T1IE = 1;
+    T1CONbits.TON = 1;       // Enable Timer
+
+
+}
+
+void setupPLL(void)
+{
+    /********************************
+     *      PLL Configuration
+     ********************************/
+    // Configure PLL prescaler, PLL postscaler, PLL divisor
+    PLLFBD=63;              // M=65
+    CLKDIVbits.PLLPOST=0;   // N2=2
+    CLKDIVbits.PLLPRE=1;    // N1=3
+
+    // Initiate Clock Switch to FRC oscillator with PLL (NOSC=0b001)
+    __builtin_write_OSCCONH(0x01);
+    __builtin_write_OSCCONL(OSCCON | 0x01);
+
+
+    // This bit of code doesn't work for some reason..
+    /**************************************
+        // Wait for Clock switch to occur
+        while (OSCCONbits.COSC!= 0b001);
+        
+
+        // Wait for PLL to lock
+        while (OSCCONbits.LOCK!= 1);
+    ***************************************/
 
 }
 
