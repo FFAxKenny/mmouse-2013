c279bd52f46c130ac85101770f0622eabc53c941
Kenny Luong <luong97@hawaii.edu>
2013-03-11 22:36:30 -1000

    added configuration file for easy usage

---
+++ TestBenches/MotorTestBench.X/Config.h
@@ -0,0 +1,16 @@
+/*
+ *  Configuration options!!
+ *
+ */
+
+
+// Set the acceleration bounds
+#define ATIMER_FIRST_BOUND  3000
+#define ATIMER_SECOND_BOUND  700
+#define ATIMER_THIRD_BOUND  300
+
+// Set the acceleration timer skip rate
+#define ATIMER_FIRST_SKIPR  0
+#define ATIMER_SECOND_SKIPR
+#define ATIMER_THIRD_SKIPR  500
+
