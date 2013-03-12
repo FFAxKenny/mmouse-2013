7f84a970ee493041c7738818cbf4cbf9155f8b8b
Kenny Luong <luong97@hawaii.edu>
2013-03-11 22:35:56 -1000

    added algorithms folder

---
+++ Algorithms/maze.c
@@ -0,0 +1,47 @@
+/*
+ *  Author: Kenny Luong
+ *  Date:   3/11/13
+ *  
+ *  Maze.c
+ *
+ *  Loads the maze into the program, and intializes each cell
+ *
+ *
+ */
+
+
+// Cell Structure
+struct Cell{
+     int floodValue;
+     int x;
+     int y;
+     bool north;
+     bool east;
+     bool south;
+     bool west;
+}
+
+
+
+int main()
+{
+    // declare the cells
+    Cell[16][16];
+    
+    // Read the maze
+        // Read the north/south walls
+
+        // Read the West/East walls
+
+   
+
+}
+
+
+
+
+
+
+
+
+
