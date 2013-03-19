24d57af0851c3cf7aa991c845d4c0a212b15d012
Kenny Luong <luong97@hawaii.edu>
2013-03-19 00:20:29 -1000

    added AtoD test bench

---
+++ TestBenches/AnalogToDigitalTestBench.X/driver.c
@@ -0,0 +1,171 @@
+/*
+ * File:   driver.c
+ * Author: kenny
+ *
+ * Created on February 9, 2013, 11:10 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <dsp.h>
+#include "config.h"
+#include "pinconfig.h"
+#include <p33Exxxx.h>
+
+//#pragma config OSCIOFNC = ON            // OSC2 Pin Function bit (OSC2 is general purpose digital I/O pin)
+_FOSCSEL(FNOSC_FRC & IESO_OFF);
+_FOSC(FCKSM_CSECMD & OSCIOFNC_ON & POSCMD_NONE);
+
+void configurePLL(void);
+void configureTimer(void);
+void Delayms(unsigned int delay);
+void initAdc1(void);
+int sampleADC(void);
+
+int ledState = 0;
+int reading = 0;
+int main()
+{
+    
+    //int number = 0;
+
+    ANSELA = 0;//configures pin B4 as digital
+    ANSELB = 0;//configures pin B4 as digital
+    ANSELAbits.ANSA0 = 1;           // Select AN0 is analog
+
+    TRISA = 0b00000000;      // Configure B Ports as output
+    TRISB = 0b00000000;      // Configure B Ports as output
+    
+    TRISBbits.TRISB15 = 1;   // Inputs
+    TRISBbits.TRISB14 = 1;
+    TRISBbits.TRISB13 = 1;
+
+    TRISAbits.TRISA0 = 1;    // Configure A0 is an input
+    
+
+    // Configure things
+    configurePLL();
+    configureTimer();
+
+    initAdc1();
+
+    PIN_Emitter1 = 0;
+    PIN_Emitter2 = 0;
+    while(1)           // Don't need this since we're only using timers..
+    {
+        reading = sampleADC();
+        while(1);
+        if(reading >= 1)
+        {
+            PIN_Emitter1 = 1;
+        }
+        else
+        {
+            PIN_Emitter1 = 0;
+        }
+        PIN_Emitter2 = 1;
+    }
+
+
+        
+
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void)
+{
+    _T1IF = 0;      // Reset the timer flag
+
+    if(ledState == 0)
+        ledState = 1;
+    else
+        ledState = 0;
+
+    /* Pins that we need to test:
+     * RA2
+     * RA3
+     * RB4
+     * RB5
+     * RB10
+     * RB9
+     * RB8
+     * RB7
+     * RB6
+     */
+     PIN_Motor1_Step = ledState;
+     PIN_Motor1_Direction = ledState;
+     PIN_Motor2_Step = ledState;
+     PIN_Motor2_Direction = ledState;
+
+}
+
+int sampleADC(void)
+{
+    int ADCValue;
+    AD1CON1bits.SAMP = 1;
+    Delayms(100);
+    AD1CON1bits.SAMP = 0;
+    while (!AD1CON1bits.DONE);
+    ADCValue = ADC1BUF0;
+    return ADCValue;
+}
+
+void configurePLL()
+{
+    // Configure PLL prescaler, PLL postscaler, PLL divisor
+    PLLFBD=63;              // M=65
+    CLKDIVbits.PLLPOST=0;   // N2=2
+    CLKDIVbits.PLLPRE=1;    // N1=3
+
+    // Initiate Clock Switch to FRC oscillator with PLL (NOSC=0b001)
+    __builtin_write_OSCCONH(0x01);
+    __builtin_write_OSCCONL(OSCCON | 0x01);
+
+    while (OSCCONbits.COSC!= 0b001);
+    while (OSCCONbits.LOCK!= 1);
+}
+
+
+void configureTimer()
+{
+    // Timer 1 configuration
+    T1CON = 0;                  // Reset T1 Configuration
+    T1CONbits.TCKPS = TIMER_RATIO;        // Set ratio to the highest
+    PR1 = TIMER_DELAY;                // Set the timer to look for
+
+    _T1IP = 1;                  
+    _T1IF = 0;
+    _T1IE = 1;
+    T1CONbits.TON = 1;          // Enable Timer
+}
+
+
+void initAdc1()
+{
+    // Set analog port to analog
+    ANSELAbits.ANSA0 = 1;           // Select AN0 is analog
+
+    // Initialize and enable ADC
+    AD1CON1 = 0x0000;
+    AD1CON2 = 0x0000;
+    AD1CON3 = 0x001F;   //..1111
+    AD1CON4 = 0x0000;
+    AD1CHS0 = 0x0005;   //..0101
+    AD1CHS123 = 0x0000;
+    AD1CSSH = 0x0000;
+    AD1CSSL = 0x0000;
+    AD1CON1bits.ADON = 1;
+    Delayms(30);
+
+
+}
+
+void Delayms(unsigned int delay)
+{
+    int i;
+    for (i = 0; i < delay; i++)
+    {
+        __asm__ volatile ("repeat #39");
+        __asm__ volatile ("nop");
+    }
+}
---
+++ TestBenches/AnalogToDigitalTestBench.X/pinconfig.h
@@ -0,0 +1,34 @@
+// Pin Configuration Definitions
+
+
+// Motor Ports
+#define PIN_Motor1_Step         LATBbits.LATB9
+#define PIN_Motor1_Direction    LATBbits.LATB5
+
+#define PIN_Motor2_Step         LATBbits.LATB8
+#define PIN_Motor2_Direction    LATBbits.LATB6
+
+// Emitters
+#define PIN_Emitter1            LATAbits.LATA2
+#define PIN_Emitter2            LATAbits.LATA3
+#define PIN_Emitter3            LATBbits.LATB4
+#define PIN_Emitter4            LATAbits.LATA4
+#define PIN_Emitter5            LATBbits.LATB7
+#define PIN_Emitter6            LATBbits.LATB10
+
+// Detectors
+#define PIN_Detector1           
+#define PIN_Detector2
+#define PIN_Detector3
+#define PIN_Detector4
+#define PIN_Detector5 
+#define PIN_Detector6
+
+// Xbee
+#define PIN_XBee_RX
+#define PIN_XBee_TX
+
+// DIP Switch Input
+#define PIN_DIP_Input1
+#define PIN_DIP_Input2
+#define PIN_DIP_Input3
