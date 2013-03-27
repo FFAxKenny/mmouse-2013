1236476a0d310c3f675984b1b7dbe6a38f1ab00f
Kenny Luong <luong97@hawaii.edu>
2013-03-27 01:38:43 -1000

    moved some motor functions over to motor.c

--- TestBenches/MotorTestBench.X/driver.c
+++ TestBenches/MotorTestBench.X/driver.c
@@ -90,22 +90,6 @@ int main()
 
 
 }
-void Motor_step(Motor *m)
-{
-    if(m->step == 1)
-        m->step = 0;
-    else
-        m->step = 1;
-    m->count++;
-}
-void Motor_enable(Motor *m)
-{
-    m->enable = 1;
-}
-void Motor_disable(Motor *m)
-{
-    m->enable = 0;
-}
 
     
 
