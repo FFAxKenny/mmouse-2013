c6429be16ac5b2b18f4b60763c9af624ddf8c8cc
Kenny Luong <luong97@hawaii.edu>
2013-05-07 10:53:37 -1000

    added project config directory

---
+++ ManapuaMouse.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ManapuaMouse.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ManapuaMouse.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=manapuamouse.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/manapuamouse.x.tar
